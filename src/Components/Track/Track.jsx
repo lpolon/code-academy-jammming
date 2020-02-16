b44636af6a55111142f3f8a49a47881a5dd60ab1
lpolon <lpolon@gmail.com>
2020-02-16 12:06:25 -0300

    added method od conditional render by props in Track.jsx

--- src/Components/Track/Track.jsx
+++ src/Components/Track/Track.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import './Track.css';
 
 export default function Track(props) {
+  const renderAction = () => {
+    const {isRemoval} = props
+
+    return (
+      <button className="Track-action">
+        {isRemoval ? '-' : '+'}
+      </button>
+    )
+  }
+
   return (
     <div className="track">
       <div className="Track-information">
         <h3>track name example</h3>
-        <p>track artist | track album</p>
+        <p>track artist example | track album example</p>
       </div>
-      <button className="Track-action"> + </button>
+      {renderAction()}
     </div>
   );
 }
